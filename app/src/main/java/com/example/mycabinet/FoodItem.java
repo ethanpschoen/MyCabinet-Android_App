1de5a020380ec90cb24fd57e3bc55270f4037792
karenpalacios <palaicoseche@oxy.edu>
2024-11-14 18:12:38 -0800

    Adding the search for foodItem - will continue to work on adding the expiration date and and otheer specifying factors

--- app/src/main/java/com/example/mycabinet/FoodItem.java
+++ app/src/main/java/com/example/mycabinet/FoodItem.java
@@ -15,14 +15,34 @@ public class FoodItem {
 
     private Context mContext;
     private List<String> mFoodItemList;
+    private List<String> mFoodNameItemList;
+    private List<String> mFoodItemNotesList;
+
+    String foodItemCount;
+    String date;
+    String time;
+    String type;
+    String description;
+
 
     public FoodItem(Context context) {
         mContext = context;
         mFoodItemList = new ArrayList<>();
+        mFoodNameItemList = new ArrayList<>();
+        mFoodItemNotesList = new ArrayList<>();
+
+        this.foodItemCount = foodItemCount;
+        this.date = date;
+        this.time = time;
+        this.type = type;
+        this.description = description;
     }
+
 
     public void addItem(String item) throws IllegalArgumentException {
         mFoodItemList.add(item);
+        mFoodNameItemList.add(item);
+        mFoodItemNotesList.add(item);
     }
 
     public String[] getItems() {
@@ -40,6 +60,25 @@ public class FoodItem {
         FileOutputStream outputStream = mContext.openFileOutput(FOODNAME, Context.MODE_PRIVATE);
         writeListToStream(outputStream);
     }
+
+    public void addNameItem(String item) throws IllegalArgumentException {
+        mFoodNameItemList.add(item);
+    }
+
+    public String[] getFoodNameItems() {
+        String[] items = new String[mFoodNameItemList.size()];
+        return mFoodNameItemList.toArray(items);
+    }
+
+    public void addNotesItem(String item) throws IllegalArgumentException {
+        mFoodNameItemList.add(item);
+    }
+
+    public String[] getFoodItemsNotes() {
+        String[] items = new String[mFoodItemNotesList.size()];
+        return mFoodItemNotesList.toArray(items);
+    }
+
 
     public void readFromFile() throws IOException {
 
@@ -65,4 +104,11 @@ public class FoodItem {
         }
         writer.close();
     }
+
+
+
+
+
 }
+
+
