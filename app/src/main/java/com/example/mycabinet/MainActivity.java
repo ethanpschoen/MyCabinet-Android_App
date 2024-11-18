2a7d0c754fc33639f9bbdc0b22e4617e7cce6bef
karenpalacios <palaicoseche@oxy.edu>
2024-11-17 20:05:48 -0800

    Color change + expiration date

--- app/src/main/java/com/example/mycabinet/MainActivity.java
+++ app/src/main/java/com/example/mycabinet/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.mycabinet;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.food_item);
+
+        Date currentTime = Calendar.getInstance().getTime();
+        String formattedDate = DateFormat.getDateInstance(DateFormat.FULL).format(currentTime);
+        String[] splitDate= formattedDate.split(",");
+
+        Log.d("myLog", currentTime.toString());
+        Log.d("myLog", formattedDate);
+
+        Log.d("myLog", splitDate[0].trim());
+        Log.d("myLog", splitDate[1].trim());
+        Log.d("myLog", splitDate[2].trim());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
