772136c7fe6feaa0beebee627a5adf741023fdea
karenpalacios <palaicoseche@oxy.edu>
2024-11-20 21:57:02 -0800

    Deleting irrelevant code for FoodItem class

--- app/src/main/java/com/example/mycabinet/FoodItem.java
+++ app/src/main/java/com/example/mycabinet/FoodItem.java
@@ -1,21 +1,12 @@
 package com.example.mycabinet;
 import android.content.Context;
-import android.util.Log;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Calendar;
+
 import java.util.List;
 
 public class FoodItem {
-    public static final String FOODNAME = "fooditem.txt";
-
     private Context mContext;
     private List<String> mFoodItemList;
     private List<String> mFoodNameItemList;
@@ -55,13 +46,6 @@ public class FoodItem {
         mFoodItemList.clear();
     }
 
-    public void saveToFile() throws IOException {
-
-        // Write list to file in internal storage
-        FileOutputStream outputStream = mContext.openFileOutput(FOODNAME, Context.MODE_PRIVATE);
-        writeListToStream(outputStream);
-    }
-
     public void addNameItem(String item) throws IllegalArgumentException {
         mFoodNameItemList.add(item);
     }
@@ -79,37 +63,6 @@ public class FoodItem {
         String[] items = new String[mFoodItemNotesList.size()];
         return mFoodItemNotesList.toArray(items);
     }
-
-
-    public void readFromFile() throws IOException {
-
-        // Read in list from file in internal storage
-        FileInputStream inputStream = mContext.openFileInput(FOODNAME);
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            mFoodItemList.clear();
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                mFoodItemList.add(line);
-            }
-        }
-        catch (FileNotFoundException ex) {
-            // Ignore
-        }
-    }
-
-    private void writeListToStream(FileOutputStream outputStream) {
-        PrintWriter writer = new PrintWriter(outputStream);
-        for (String item : mFoodItemList) {
-            writer.println(item);
-        }
-        writer.close();
-    }
-
-
-
-
-
 }
 
 
