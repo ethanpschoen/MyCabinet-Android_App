2a3f315a6c8a76ace8a3ef548a9f5c3171a4195d
Ethan Schoen <ethanparkerschoen@gmail.com>
2024-11-26 00:14:38 -0800

    AddItemActivity progress

--- app/src/main/java/com/example/mycabinet/AddItemActivity.java
+++ app/src/main/java/com/example/mycabinet/AddItemActivity.java
@@ -1,6 +1,8 @@
 package com.example.mycabinet;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AddItemActivity extends AppCompatActivity {
 
+    EditText foodName;
+    EditText foodNotes;
+    EditText foodMonth;
+    EditText foodDay;
+    EditText foodYear;
+    Button addButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        foodName = findViewById(R.id.food_name);
+        foodNotes = findViewById(R.id.food_notes);
+        foodMonth = findViewById(R.id.food_month);
+        foodDay = findViewById(R.id.food_day);
+        foodYear = findViewById(R.id.food_year);
+        addButton = findViewById(R.id.add_item_button);
+
+        addButton.setOnClickListener(view -> {
+            String name = foodName.getText().toString();
+            String notes = foodNotes.getText().toString();
+            String month = foodMonth.getText().toString();
+            String day = foodDay.getText().toString();
+            String year = foodYear.getText().toString();
+
+        });
     }
 }
