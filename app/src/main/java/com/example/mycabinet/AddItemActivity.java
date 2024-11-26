0537736471f30275acde26e47307169eabf0d299
karenpalacios <palaicoseche@oxy.edu>
2024-11-26 00:16:51 -0800

    re-adding the acitvity_main file so that it has sectionlist and settings button + fragment for navigation

---
+++ app/src/main/java/com/example/mycabinet/AddItemActivity.java
@@ -0,0 +1,31 @@
+package com.example.mycabinet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_item);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void foodItem(View view) {
+        Intent i = new Intent(getApplicationContext(), FoodSection.class);
+        startActivity(i);
+    }
+}
