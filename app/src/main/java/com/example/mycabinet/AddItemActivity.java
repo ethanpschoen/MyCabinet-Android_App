96069ca12eca576b62e798263182acf2c595c8c7
Ethan Schoen <ethanparkerschoen@gmail.com>
2024-11-25 23:38:57 -0800

    Testing intents

---
+++ app/src/main/java/com/example/mycabinet/AddItemActivity.java
@@ -0,0 +1,18 @@
+package com.example.mycabinet;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
